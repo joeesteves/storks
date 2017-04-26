b22827e82021eb87b75e975d635d66cd0c68c96f
ponyesteves <ajesteves@gmail.com>
2017-04-26 12:42:31 -0300

    Auth Basico funcionando! Guarda los datos de acceso en session.json

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const fs = require('fs'),
   cors = require('cors'),
   express = require('express'),
   app = express(),
-  authRedirectUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5894928571543101`
+  authRedirectUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5894928571543101`,
+  request = require('request')
 
 
 https.createServer({
@@ -18,8 +19,29 @@ app.get('/', (req, res) => {
   res.redirect(authRedirectUrl)
 })
 
-app.get('/home', function (req, res) {
-  res.header('Content-type', 'text/html')
-  fs.writeFileSync('session.json', JSON.stringify(req.query))
-  return res.end(`<h1>Hello, Secure Worlds! ${JSON.stringify(req.params)}</h1>`);
+app.get('/auth', function (req, res) {
+  if (req.query.code) {
+    request.post('https://api.mercadolibre.com/oauth/token', {
+      form: {
+        grant_type: 'authorization_code',
+        client_id: '5894928571543101',
+        client_secret: 'NI64BVwyCD2TzZmWUY3uNo3slZBxte7a',
+        code: req.query.code,
+        redirect_uri: 'https://storks.elcaminosoftware.com:3000/auth'
+      }
+    }, (tokenError, tokenResponse, tokenBody) => {
+      fs.writeFileSync('session.json', JSON.stringify(JSON.parse(tokenBody)))
+      res.redirect('/home')
+    })
+  } else {
+
+    return res.send('Ingresando a la app...');
+  }
 })
+
+app.get('/home', function (req, res) {
+  const sessionData = JSON.parse(fs.readFileSync('session.json','utf8'))
+  console.log(sessionData.access_token)
+  return res.send(`<h1>Hola este es su access token! ${sessionData.access_token}</h1>`);
+})
+
