081326f60577267744ab311437467d0ae3e98a8b
ponyesteves <ajesteves@gmail.com>
2017-05-03 12:20:50 -0300

    Envio de mails

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const fs = require('fs'),
   http = require('http'),
   https = require('https'),
+  request = require('request'),
   cors = require('cors'),
   express = require('express'),
   app = express(),
-  authRedirectUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5894928571543101`,
-  request = require('request'),
   RF = require('ramda-fantasy'),
+  R = require('ramda'),
+  myAppKeys = { client_id: '5894928571543101', cliente_secret: 'NI64BVwyCD2TzZmWUY3uNo3slZBxte7a', redirect_uri: 'https://storks.elcaminosoftware.com:3000/auth' },
+  authRedirectUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${myAppKeys.client_id}`,
   bodyParser = require('body-parser'),
   Maybe = require('ramda-fantasy').Maybe,
   sendMail = require('./sendMail').sendMail,
@@ -15,6 +17,7 @@ const fs = require('fs'),
 
 //AutoCompact every 20 min
 db.persistence.setAutocompactionInterval(1200000)
+// 
 
 let session = JSON.parse(fs.readFileSync('session.json'))
 
@@ -41,10 +44,10 @@ app.get('/auth', function (req, res) {
       request.post('https://api.mercadolibre.com/oauth/token', {
         form: {
           grant_type: 'authorization_code',
-          client_id: '5894928571543101',
-          client_secret: 'NI64BVwyCD2TzZmWUY3uNo3slZBxte7a',
+          client_id: myAppKeys.client_id,
+          client_secret: myAppKeys.cliente_secret,
           code,
-          redirect_uri: 'https://storks.elcaminosoftware.com:3000/auth'
+          redirect_uri: myAppKeys.redirect_uri
         }
       }, (tokenError, tokenResponse, tokenBody) => {
         console.log("tokenError" + tokenError)
@@ -80,6 +83,7 @@ app.post('/producto', (req, res) => {
 })
 
 app.post('/pago', (req, res) => {
+  // {"message":"invalid_token","cause":[],"error":"not_found","status":401} 
   // req.query {topic: 'payment', id: ## }
   // Get PaymentData
   // https://api.mercadopago.com/collections/notifications/[ID]?access_token=[ACCESS_TOKEN]
@@ -91,28 +95,133 @@ app.post('/pago', (req, res) => {
   // Busco el producto y la licencia //
   // Pasar toda esa data al mail para armar un mail asi
   // Estimado
-  db.findOne({ id: 191083848 }, (e, pDoc) => {
-    getLicencia(pDoc)
-    db.findOne({ id: 'configuracion' }, (e, doc) => {
-      sendMail((error, info) => {
-        const { data, status } = error ? { data: error, status: 500 } : { data: info, status: 200 }
-        res.status(status).send(data)
-      }, doc, pDoc.template, getLicencia(pDoc))
+  procesarPagos(req, res)
 
+})
+
+const procesarPagos = (req, res) => {
+  let datos;
+  getPaymentData(req)
+    .then(pay => {
+      datos = {
+        cliente: pay.payer,
+        orderId: pay.order_id
+      }
+      return getOrderData(pay.order_id)
+    })
+    .then(order => {
+      console.log(JSON.stringify(order.order_items[0].item.title))
+
+      datos['producto'] = {
+        id: order.order_items[0].item.id,
+        title: order.order_items[0].item.title
+      }
+      db.findOne({ id: datos.producto.id }, (e, pDoc) => {
+        console.log("LICENCIA" + datos.producto.id)
+        console.log(pDoc)
+        if (!pDoc) return res.sendStatus(200)
+        if (noMail(pDoc)) { // Cuando no hay template se devuelve 200 y no se envia mail
+          console.log('NO MMAIL')
+          return res.sendStatus(200)
+        }
+        db.findOne({ id: 'configuracion' }, (e, conf) => {
+
+          sendMail((error, info) => {
+            const { data, status } = error ? { data: error, status: 500 } : { data: info, status: 200 }
+            res.status(status).send(data)
+          }, conf, Object.assign({}, getLicencia(pDoc),
+            {
+              template: pDoc.template,
+              nombre: datos.cliente.first_name,
+              apodo: datos.cliente.nickname,
+              email: datos.cliente.email,
+              producto: datos.producto.title
+            })
+          )
+        })
+      })
+    })
+    .catch(e => {
+      console.log(e.status)
+      if (e.status === 401) {
+        console.log("Refresh Token")
+        refreshToken()
+          .then(() => {
+            procesarPagos(req, res)
+          })
+      } else {
+        // console.log(e)
+        res.sendStatus(e.status)
+      }
+    })
+
+}
+const getPaymentData = (req) => {
+  console.log(req.query)
+  return new Promise((resolve, reject) => {
+    request(`https://api.mercadopago.com/collections/notifications/${req.query.id}?access_token=${session.access_token}`,
+      (err, res, body) => {
+        console.log('PAYMENT')
+        if (err || (res && res.statusCode >= 400)) return reject({ res, status: res.statusCode })
+        console.log("LLEGA??")
+        resolve(JSON.parse(body).collection)
+      })
+  })
+}
+
+const getOrderData = (orderId) => {
+  return new Promise((resolve, reject) => {
+    request(`https://api.mercadolibre.com/orders/${orderId}?access_token=${session.access_token}`,
+      (err, res, body) => {
+        console.log('ORDER')
+        if (err || (res && res.statusCode >= 400)) return reject({ res, status: res.statusCode })
+        resolve(JSON.parse(body))
+      })
+  })
+}
+
+app.get('/refresh', (req, res) => {
+  refreshToken()
+    .then((body) => {
+      res.send(body)
+    })
+
+})
+
+
+const refreshToken = () => {
+  return new Promise((resolve, reject) => {
+    request(`https://api.mercadolibre.com/oauth/token?grant_type=refresh_token&client_id=${myAppKeys.client_id}&client_secret=${myAppKeys.cliente_secret}&refresh_token=${session.refresh_token}`, {
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'content-type': 'application/json'
+      }
+    }, (err, res, body) => {
+      if (err || (res && res.statusCode >= 400)) return reject({ error: err, res })
+      const newSession = Object.assign({}, JSON.parse(body), { user_id: session.user_id })
+      fs.writeFileSync('session.json', JSON.stringify(newSession))
+      session = newSession
+      resolve(body)
     })
 
   })
-})
+}
+
 const getLicencia = (producto) => {
   const lic = producto.licencias[0]
+  if (!lic) return {}
   if (lic.cantidad == 1) {
     licencias = [...producto.licencias.slice(1)]
   } else {
-    licencias = [Object.assign({}, lic, { cantidad: lic.cantidad -  1 }), ...producto.licencias.slice(1)]
+    licencias = [Object.assign({}, lic, { cantidad: lic.cantidad - 1 }), ...producto.licencias.slice(1)]
   }
   db.update({ id: producto.id }, { $set: { licencias } })
   return lic
 }
+
+const noMail = (producto) => R.isEmpty(producto.template)
+
 
 app.post('/configuracion', (req, res) => {
   db.findOne({ id: 'configuracion' }, (err, doc) => {
--- server/sendMail.js
+++ server/sendMail.js
@@ -1,19 +1,19 @@
 const nodemailer = require('nodemailer')
 
-const sendMail = (cb, conf, template, lic) => {
+
+const sendMail = (cb, conf, data = {}) => {
+  console.log(JSON.stringify(conf))
   transporter = nodemailer.createTransport(`smtps://${conf.email.replace('@', '%40')}:${conf.password}@${conf.smtp}`),
     mailOptions = {
       from: '"Storks Compras" <admin@elcaminosoftware.com>', // sender address
-      to: 'ajesteves@gmail.com', // list of receivers
+      to: data.email, // list of receivers
       subject: 'Gracias por su compra ✔', // Subject line
-      text: 'Prueba', // plaintext body
-      html: parseTemplate(template, lic)
+      text: 'Mail diseñado para verse con soporte HTML', // plaintext body
+      html: parseTemplate(data)
     }
   transporter.sendMail(mailOptions, cb)
 }
 
-const parseTemplate = (template, lic) => ['codigo', 'downloadLink'].reduce((prev, curr) => prev.replace(`@${curr}`, lic[curr]), template)
-
-
+const parseTemplate = (data) => ['codigo', 'downloadLink'].reduce((prev, curr) => prev.replace(`@${curr}`, data[curr]), data.template)
 
 module.exports = { sendMail }
