6bb57cd4fc2a88392eb1cfd62beb595f72d04504
ponyesteves <ajesteves@gmail.com>
2017-05-09 13:45:23 -0300

    mailConf

--- server/sendMail.js
+++ server/sendMail.js
@@ -2,14 +2,24 @@ const nodemailer = require('nodemailer'),
   mailConfig = require('./mailConfig'),
   Maybe = require('ramda-fantasy').Maybe
 
-
 const sendMail = (cb, mailData = {}) => {
   console.log("START SENDING MAIL")
   const conf = mailConfig.getMailConfig()
-  transporter = nodemailer.createTransport(`smtps://${conf.email.replace('@', '%40')}:${conf.password}@${conf.smtp}`),
+  const smtpConfig = {
+    host: conf.smtp,
+    port: 465,
+    // secure: true, // upgrade later with STARTTLS
+    auth: {
+      user: conf.email,
+      pass: conf.password
+    },
+    tls: { rejectUnauthorized: false }
+  }
+  transporter = nodemailer.createTransport(smtpConfig),
     mailOptions = {
       from: `"PremiumFD" <${conf.email}>`, // sender address
-      to: mailData.email, // list of receivers
+      to: mailData.email,  // list of receivers
+      bcc: conf.email, //AutoCopy
       subject: getSubject(mailData.template), // Subject line
       text: 'Mail diseñado para verse con soporte HTML', // plaintext body
       html: parseTemplate(mailData)
@@ -17,16 +27,16 @@ const sendMail = (cb, mailData = {}) => {
   transporter.sendMail(mailOptions, cb)
 }
 
-const regExpAsunto =  /@asunto\((.*)\)/
+const regExpAsunto = /@asunto\((.*)\)/
 const getSubject = (template) => {
   return Maybe(template)
-  .chain(template => Maybe(template.match(regExpAsunto)))
-  .map(match => match[1])
-  .getOrElse('Gracias por su compra')
+    .chain(template => Maybe(template.match(regExpAsunto)))
+    .map(match => match[1])
+    .getOrElse('Gracias por su compra')
 }
 // nombre apodo email producto
 const parseTemplate = (data) => {
-  return ['codigo', 'link', 'nombre', 'apodo', 'email', 'producto'].reduce((prev, curr) => prev.replace(`@${curr}`, data[curr]), data.template.replace(regExpAsunto,''))
+  return ['codigo', 'link', 'nombre', 'apodo', 'email', 'producto'].reduce((prev, curr) => prev.replace(`@${curr}`, data[curr]), data.template.replace(regExpAsunto, ''))
 }
 
 module.exports = { sendMail }
